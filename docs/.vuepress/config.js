994c305c099c2cb1bae31408f64144e7b58a26e1
root <root@root.ru>
2022-10-17 22:09:40 +0300

    2022.10.17

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+
+module.exports = {
+    title: 'Wiki | SaintPlace',
+    description: ' ',
+    head: [
+        ['meta', { name: 'theme-color', content: '#9f6bba' }],
+        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+        ['link', { rel: 'icon', href: '/favicon.svg' }]
+    ],
+    locales: {
+        '/': {
+            lang: 'ru-RU'
+        }
+    },
+    markdown: {
+        extractHeaders: [ 'h2', 'h3', 'h4', 'h5' ]
+    },
+    themeConfig: {
+        editLinks: false,
+        docsDir: '',
+        lastUpdated: false,
+        search: true,
+        nav: [
+            {
+                text: 'Инструкция',
+                link: '/guide/',
+                // items: [
+                //   {
+                //     text: 'Регистрация',
+                //     link: '/guide/',
+                //     items: [
+                //       {
+                //         text: 'Выбор тарифа',
+                //         link: '/guide/#выбор-тарифа'
+                //       },
+                //       {
+                //         text: 'Форма регистрации',
+                //         link: '/guide/#форма-регистрации'
+                //       }
+                //     ]
+                //   }
+                // ]
+            },
+            {
+                text: 'SaintPlace',
+                link: 'https://saint-place.ru'
+            }
+        ],
+        sidebar: {
+            '/guide/': [
+                {
+                    title: 'Инструкция',
+                    collapsable: true,
+                    sidebarDepth: 5,
+                    children: [
+                        '',
+                        'product_import',
+                        'product_add'
+                    ]
+                }
+            ],
+        }
+    },
+    plugins: [
+        ['@vuepress/back-to-top', true],
+        ['@vuepress/medium-zoom', true]
+    ],
+    extraWatchFiles: ['.vuepress/config/**'],
+}
---
+++ docs/index.md
@@ -0,0 +1,25 @@
+---
+home: true
+heroImage: logo.png
+actionText: Инструкция →
+actionLink: /guide/
+---
+
+<div class="features">
+  <div class="feature">
+    <h2>Продавцы</h2>
+    <p>text</p>
+  </div>
+  <div class="feature">
+    <h2>Покупатели</h2>
+    <p>text</p>
+  </div>
+  <div class="feature">
+    <h2>Техническая поддержка</h2>
+    <p>text</p>
+  </div>
+</div>
+
+::: slot footer
+Сделано с ❤️
+:::
